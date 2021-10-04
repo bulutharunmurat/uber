f87b9714ca022970a9ab07afc7d33dcb059387f9
bulutharunmurat <bulutharunmurat@gmail.com>
2021-10-04 22:24:42 +0300

    Add driver controller

--- src/main/java/uber/controller/DriverController.java
+++ src/main/java/uber/controller/DriverController.java
@@ -1,12 +1,13 @@
 package uber.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import uber.entity.Driver;
 import uber.service.DriverService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -18,5 +19,15 @@ public class DriverController {
     @PostMapping("/drivers")
     public Driver saveDriver(@RequestBody Driver driver){
         return driverService.save(driver);
+    }
+
+    @GetMapping("/drivers")
+    public List<Driver> getAllDrivers(){
+        return driverService.findAll();
+    }
+
+    @GetMapping("/drivers/{id}")
+    public ResponseEntity<Driver> findStudentById(@PathVariable Integer id){
+        return new ResponseEntity<>(driverService.findById(id), HttpStatus.OK);
     }
 }
--- src/main/java/uber/service/DriverService.java
+++ src/main/java/uber/service/DriverService.java
@@ -5,6 +5,10 @@ import org.springframework.stereotype.Service;
 import uber.entity.Driver;
 import uber.repository.DriverRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class DriverService {
 
@@ -13,5 +17,16 @@ public class DriverService {
 
     public Driver save(Driver driver){
         return driverRepository.save(driver);
+    }
+
+    public List<Driver> findAll(){
+        List<Driver> drivers = new ArrayList<>();
+        Iterable<Driver> driverIterable = driverRepository.findAll();
+        driverIterable.iterator().forEachRemaining(drivers::add);
+        return drivers;
+    }
+
+    public Driver findById(Integer id){
+        return driverRepository.findById(id).get();
     }
 }
